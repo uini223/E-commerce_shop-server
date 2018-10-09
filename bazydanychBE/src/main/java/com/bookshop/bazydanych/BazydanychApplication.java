1962475c62fae46944d879c122304c0186a02c56
Emil Bartnik <bartnik.emil@gmail.com>
2018-10-09 23:11:31 +0200

    Initial commit

---
+++ bazydanychBE/src/main/java/com/bookshop/bazydanych/BazydanychApplication.java
@@ -0,0 +1,12 @@
+package com.bookshop.bazydanych;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BazydanychApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BazydanychApplication.class, args);
+    }
+}
---
+++ bazydanychBE/src/test/java/com/bookshop/bazydanych/BazydanychApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bookshop.bazydanych;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BazydanychApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
