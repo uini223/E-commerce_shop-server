00036d56f54af6628fa3de3def5ef6e2acbcca3e
Emil Bartnik <bartnik.emil@gmail.com>
2018-11-08 09:57:55 +0100

    * cors improvements

--- src/main/java/com/bookshop/bazydanych/security/SecurityConfiguration.java
+++ src/main/java/com/bookshop/bazydanych/security/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.bookshop.bazydanych.security;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -18,6 +19,9 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.NegatedRequestMatcher;
 import org.springframework.security.web.util.matcher.OrRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
@@ -60,6 +64,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 			.authenticationProvider(provider)
 			.addFilterBefore(restAuthenticationFilter(), AnonymousAuthenticationFilter.class)
 			.authorizeRequests()
+			.antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
 			.requestMatchers(PROECTED_URLS)
 			.authenticated()
 			.and()
@@ -89,6 +94,18 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		final FilterRegistrationBean registration = new FilterRegistrationBean(filter);
 		registration.setEnabled(false);
 		return registration;
+	}
+
+	@Bean
+	CorsFilter corsFilter() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedOrigin("*"); // @Value: http://localhost:8080
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("*");
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
 	}
 
 	@Bean
--- src/main/java/com/bookshop/bazydanych/user/webui/PublicUserController.java
+++ src/main/java/com/bookshop/bazydanych/user/webui/PublicUserController.java
@@ -3,6 +3,8 @@ package com.bookshop.bazydanych.user.webui;
 import com.bookshop.bazydanych.security.UserAuthenticationService;
 import com.bookshop.bazydanych.user.application.UserSecurityService;
 import com.bookshop.bazydanych.user.readmodel.UserDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("public/users")
 public class PublicUserController {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(PublicUserController.class);
 
 	private UserAuthenticationService authenticationService;
 	private UserSecurityService userSecurityService;
@@ -29,6 +33,7 @@ public class PublicUserController {
 
 	@PostMapping(value = "/login")
 	public String login(@RequestBody final UserDTO user) {
+		LOGGER.debug("Login request !");
 		return authenticationService.login(user.getLogin(), user.getPassword())
 			.orElseThrow(() -> new RuntimeException("invalid login and/or password"));
 	}
