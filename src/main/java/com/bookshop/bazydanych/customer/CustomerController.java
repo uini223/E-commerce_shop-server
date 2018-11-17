83c5ef07fc46da445df91da47226394e0cac9a25
wangobango <33557270+wangobango@users.noreply.github.com>
2018-11-17 19:51:31 +0100

    Update CustomerController.java

--- src/main/java/com/bookshop/bazydanych/customer/CustomerController.java
+++ src/main/java/com/bookshop/bazydanych/customer/CustomerController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/customer")
+@RequestMapping("/customers")
 public class CustomerController  {
 
 	private final CustomerService customerService;
