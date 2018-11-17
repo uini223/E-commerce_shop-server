a0288eae626ab2aa906edc0309b4de7a2792485b
wangobango <puch003@wp.pl>
2018-11-17 20:18:14 +0100

    changed location to locations

--- src/main/java/com/bookshop/bazydanych/location/LocationController.java
+++ src/main/java/com/bookshop/bazydanych/location/LocationController.java
@@ -1,15 +1,11 @@
 package com.bookshop.bazydanych.location;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/location")
+@RequestMapping("/locations")
 public class LocationController {
 
 	private final LocationService locationService;
--- src/main/resources/sqls/create_example_table_with_content.sql
+++ src/main/resources/sqls/create_example_table_with_content.sql
@@ -36,3 +36,5 @@ VALUES ('karta', 'kg', 'Dupsko', 20, '1', 1.0, '1',1,1,1);
 
 INSERT INTO products(name, unit, producent, stock, status, price, description,currency_id,platform_id,category_id)
 VALUES ('emil ma małego', 'kg', 'Dupsko', 20, '1', 1.0, '1',1,1,1);
+
+SELECT * FROM locations ;
