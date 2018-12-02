cc08a94239f4849b3f2c28a48657c74813cee9bb
wangobango <puch003@wp.pl>
2018-12-02 19:28:47 +0100

    new products

--- src/main/resources/sqls/create_example_table_with_content.sql
+++ src/main/resources/sqls/create_example_table_with_content.sql
@@ -11,25 +11,28 @@ INSERT INTO shops(name, owner_name)
 VALUES ('gowny sklep','ramon');
 
 INSERT INTO platforms(name, status, shop_id, location_id)
-VALUES ('dupa_platform','active',1,1);
+VALUES ('Main Platform','active',1,1);
 
 INSERT INTO currencies
 VALUES (1, 'złoty', 'PLN', 1.0);
 
 INSERT INTO categories(name,description)
-VALUES ('gowna','gunfo');
+VALUES ('Belles-lettres','');
 
 INSERT INTO categories(name,description)
-VALUES ('nie guwna','fajna nazwa kategorii');
+VALUES ('Psychological','');
+
+INSERT INTO categories(name,description)
+VALUES ('Fantasy','');
 
 INSERT INTO products(name, unit, producent, stock, status, price, description,currency_id,platform_id,category_id)
-VALUES ('dupa', 'kg', 'Dupsko', 20, '1', 1.0, '1',1,1,1);
+VALUES ('Murakami - Norwegian Wood', 'szt', 'Book Publisher', 20, '1', 1.0, '1',1,1,1);
 
 INSERT INTO products(name, unit, producent, stock, status, price, description,currency_id,platform_id,category_id)
-VALUES ('test', 'kg', 'Dupsko', 20, '1', 1.0, '1',1,1,1);
+VALUES ('Dostoevsky - Brothers Karamazhow', 'kg', 'Book Publisher', 20, '1', 1.0, '1',1,1,2);
 
 INSERT INTO products(name, unit, producent, stock, status, price, description,currency_id,platform_id,category_id)
-VALUES ('karta', 'kg', 'Dupsko', 20, '1', 1.0, '1',1,1,1);
+VALUES ('Dostoevsky - Idiot', 'szt', 'Book Publisher', 20, '1', 1.0, '1',1,1,2);
 
 INSERT INTO products(name, unit, producent, stock, status, price, description,currency_id,platform_id,category_id)
-VALUES ('emil ma dużego :)', 'kg', 'Dupsko', 20, '1', 1.0, '1',1,1,2);
+VALUES ('Dick - Blade Runner', 'szt', 'Book Publisher', 20, '1', 1.0, '1',1,1,3);
